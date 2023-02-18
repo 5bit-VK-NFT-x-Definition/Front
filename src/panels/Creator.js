8efdf469a5e5e955b7ba761f6b8e1fb0f55b275c
s1zzer <chepko_daniil@mail.ru>
2023-02-18 16:02:50 +0300

    Modifite

--- src/panels/Creator.js
+++ src/panels/Creator.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 import CustomPanelHeader from "../components/CustomPanelHeader/CustomPanelHeader";
+import CustomPanelHeaderCustomer from "../components/CustomPanelHeaderCustomer/CustomPanelHeaderCustomer";
 
 import {
   Panel,
@@ -33,7 +34,7 @@ const Creator = ({ id, go, onBackClick }) => {
 
   return (
     <Panel id={id}>
-      <CustomPanelHeader name="Content Producer" />
+      <CustomPanelHeaderCustomer name="Content Producer" />
       <PanelHeaderBack onClick={go} data-to="home" />
       <FormLayout>
         <FormItem top="Название события">
