171d1265f0e18e9d8a09298d70540948985c12c2
D1scoDancer <apshulikov@edu.hse.ru>
2023-02-18 07:13:22 +0300

    wallet connected

--- src/components/WalletConnect/WalletConnect.js
+++ src/components/WalletConnect/WalletConnect.js
@@ -4,13 +4,49 @@ import "./WalletConnect.css";
 import { Button } from "@vkontakte/vkui";
 
 const WalletConnect = () => {
-  const [buttonText, setButtonText] = useState("Connect Wallet");
+  const [connButtonText, setConnButtonText] = useState("Connect Wallet");
 
-  const test = () => {
-    console.log("test");
+  const connectWalletHandler = () => {
+    if (window.ethereum && window.ethereum.isMetaMask) {
+      console.log("MetaMask Here!");
+
+      window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((result) => {
+          accountChangedHandler(result[0]);
+          setConnButtonText(
+            result[0].substring(0, 5) + "..." + result[0].substring(39)
+          );
+        })
+        .catch((error) => {
+          setErrorMessage(error.message);
+        });
+    } else {
+      console.log("Need to install MetaMask");
+      setErrorMessage("Please install MetaMask browser extension to interact");
+    }
   };
 
-  return <Button onClick={() => setButtonText("onClick")}>{buttonText}</Button>;
+  // update account, will cause component re-render
+  const accountChangedHandler = (newAccount) => {
+    setConnButtonText(
+      newAccount[0].substring(0, 5) + "..." + newAccount[0].substring(39)
+    );
+  };
+
+  const chainChangedHandler = () => {
+    window.location.reload();
+  };
+
+  window.ethereum.on("accountsChanged", accountChangedHandler);
+
+  window.ethereum.on("chainChanged", chainChangedHandler);
+
+  return (
+    <>
+      <Button onClick={connectWalletHandler}>{connButtonText}</Button>
+    </>
+  );
 };
 
 export default WalletConnect;
