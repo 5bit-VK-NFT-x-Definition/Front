43170c77c8a4c9130ea1c1e00cb237f3f9a3265d
s1zzer <chepko_daniil@mail.ru>
2023-02-18 13:00:06 +0300

    Fixing again

--- src/panels/Customer.js
+++ src/panels/Customer.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import { Panel, Button, Div } from "@vkontakte/vkui";
+import CustomPanelHeaderCustomer from "../components/CustomPanelHeaderCustomer/CustomPanelHeaderCustomer";
 
 import {
   PanelHeader,
@@ -11,29 +12,71 @@ import {
   Cell,
   Avatar,
   CardGrid,
-  Card
+  Card,
+  CellButton,
+  useAdaptivityConditionalRender,
+  PopoutWrapper,
+  ModalDismissButton,
 } from "@vkontakte/vkui";
-import CustomPanelHeaderCustomer from "../components/CustomPanelHeaderCustomer/CustomPanelHeaderCustomer";
 
 const Customer = ({ id, go }) => (
   <Panel id={"Customer"}>
     <PanelHeader before={<PanelHeaderBack onClick={() => setActiveView('home')} />}></PanelHeader>
     <CustomPanelHeaderCustomer/>
-    <Group description="Внутри NFT">
+    <Group description="NFT">
       <CardGrid size="s">
         <Card>
-          <div style={{ paddingBottom: '92%' }} />
+          <div style={{ paddingBottom: '50%' }} />
+          <CellButton onClick={"r"}>Использовать</CellButton>
         </Card>
         <Card>
-          <div style={{ paddingBottom: '92%' }} />
+          <div style={{ paddingBottom: '50%' }} />
+          <CellButton onClick={"R"}>Использовать</CellButton>
         </Card>
         <Card>
-          <div style={{ paddingBottom: '92%' }} />
+          <div style={{ paddingBottom: '50%' }} />
+          <CellButton onClick={"R"}>Использовать</CellButton>
+        </Card>
+      </CardGrid>
+      <CardGrid size="s">
+        <Card>
+          <div style={{ paddingBottom: '50%' }} />
+          <CellButton onClick={"R"}>Использовать</CellButton>
+        </Card>
+        <Card>
+          <div style={{ paddingBottom: '50%' }} />
+          <CellButton onClick={"R"}>Использовать</CellButton>
+        </Card>
+        <Card>
+          <div style={{ paddingBottom: '50%' }} />
+          <CellButton onClick={"R"}>Использовать</CellButton>
         </Card>
       </CardGrid>
     </Group>
   </Panel>
 );
+
+const CustomPopout = ({ onClose }) => {
+  const { sizeX } = useAdaptivityConditionalRender();
+  return (
+    <PopoutWrapper onClick={onClose}>
+      <div
+        style={{
+          backgroundColor: 'var(--vkui--color_background_content)',
+          borderRadius: 8,
+          position: 'relative',
+          padding: '12px',
+        }}
+      >
+        <h4>Кастомное модальное окно</h4>
+
+        {sizeX.regular && (
+          <ModalDismissButton className={sizeX.regular.className} onClick={onClose} />
+        )}
+      </div>
+    </PopoutWrapper>
+  );
+};
 
 
 Customer.propTypes = {
