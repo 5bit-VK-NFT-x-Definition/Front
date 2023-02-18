dca477cd6f935a583cead50a0255c50ef3829241
s1zzer <chepko_daniil@mail.ru>
2023-02-18 09:40:44 +0300

    fix_my_page(not end)

---
+++ src/components/CustomPanelHeaderCustomer/CustomPanelHeaderCustomer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./CustomPanelHeaderCustomer.css";
+
+import {
+  Panel,
+  PanelHeader,
+  Header,
+  Button,
+  Group,
+  Cell,
+  Div,
+  Avatar,
+  PanelHeaderBack,
+} from "@vkontakte/vkui";
+
+const CustomPanelHeader = () => {
+  return (
+    <panel id ="Customer">
+        <PanelHeader before={<PanelHeaderBack onClick={"home"} />}>
+            VK NFT-билеты Definition
+        </PanelHeader>
+    </panel>
+    );
+};
+
+export default CustomPanelHeader;
--- src/panels/Customer.js
+++ src/panels/Customer.js
@@ -1,21 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import { Panel, Button, Div } from "@vkontakte/vkui";
+import CustomPanelHeader from "../components/CustomPanelHeaderCustomer/CustomPanelHeaderCustomer";
+
 import {
-  Panel,
   PanelHeader,
   Header,
-  Button,
   Group,
   Cell,
-  Div,
   Avatar,
 } from "@vkontakte/vkui";
 
 const Customer = ({ id, go }) => (
   <Panel id={id}>
-    <PanelHeader>Customer</PanelHeader>
-    abc
+    <CustomPanelHeader />
   </Panel>
 );
 
